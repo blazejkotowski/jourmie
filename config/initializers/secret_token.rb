3d68d879b0d25835f73b035e93eb1b8ac21dc68d
Błażej Kotowski <blaziko@gmail.com>
2013-02-25 16:16:53 +0100

    initing rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jourmie::Application.config.secret_token = 'c3e301e51da37dcbc7ac62395c10827418d767da96c0075a9fa258307e6a5581618dea65f84fc3dd13dfb76413e1815d70f34b1bf2223c2a0fba94147738b1a8'
