06fe823936eb94b9a585513c2c49621d35aac00c
Błażej Kotowski <blaziko@gmail.com>
2013-03-02 17:17:29 +0100

    perform authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
+  
+  if Rails.env.production?  
+    http_basic_authenticate_with :name => "memories", :password => "memories"
+  end
+  
   protect_from_forgery
   helper DeviseHelper
+  
 end
