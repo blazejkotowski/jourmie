f9510eab78a978980e2fa01f43b19f3f5a6e865a
Błażej Kotowski <blaziko@gmail.com>
2013-03-04 13:46:03 +0100

    there's no image

--- db/migrate/20130304121405_remove_image_from_user_profile.rb
+++
@@ -1,9 +0,0 @@
-class RemoveImageFromUserProfile < ActiveRecord::Migration
-  def up
-    remove_column :user_profiles, :image
-  end
-
-  def down
-    add_column :user_profiles, :image, :string
-  end
-end
