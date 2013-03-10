fbb9f6603656a9e7226b67585fd96ccfd9e540fb
Błażej Kotowski <blaziko@gmail.com>
2013-03-10 13:52:25 +0100

    its Jourmie.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,9 @@
-root = "/home/deployer/apps/kipscore/current"
+root = "/home/deployer/apps/jourmie/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.kipscore.sock"
+listen "/tmp/unicorn.jourmie.sock"
 worker_processes 2
 timeout 30
