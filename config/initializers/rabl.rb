6dc2b80b365e743fa0ab6811c053683b127a522e
Błażej Kotowski <blaziko@gmail.com>
2013-04-30 14:28:15 +0200

    pretty json displaying

--- config/initializers/rabl.rb
+++ config/initializers/rabl.rb
@@ -1,4 +1,11 @@
+class PrettyJson
+  def self.dump(object)
+    JSON.pretty_generate(object, {:indent => "  "})
+  end
+end
+
 Rabl.configure do |config|
   config.include_json_root = false
   config.include_child_root = false
+  config.json_engine = PrettyJson if Rails.env.development?
 end
