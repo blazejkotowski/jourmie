bb483d3251c030d187518bb01b1edf7a2b25fbf0
Błażej Kotowski <blaziko@gmail.com>
2013-05-15 12:56:55 +0200

    user should be able to comment if is participant

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
-  load_and_authorize_resource :content_piece, :instance_name => :commentable
-  load_and_authorize_resource :comment, :through => :content_piece, :except => :create
+  load_and_authorize_resource :content_piece, :instance_name => :commentable, :except => [:create]
+  load_and_authorize_resource :comment, :through => :content_piece, :except => [:create]
   
   respond_to :json
 
@@ -11,6 +11,7 @@ class CommentsController < ApplicationController
   end
 
   def create
+    @commentable = ContentPiece.find(params[:commentable_id])
     @comment = @commentable.comments.build :user_id => current_user.id, :text => params[:content]
     @comment.save
   end
