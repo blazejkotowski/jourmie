667ca0a14657636b88adc9f75a10b0607432a1d3
Błażej Kotowski <blaziko@gmail.com>
2013-05-15 12:07:41 +0200

    syntax and display errors on production

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -25,7 +25,7 @@ class Ability
     # Comments abilities
     can :manage, Comment, :user_id => user.id
     can :create, Comment do |comment|
-      comment.user_id == user.id || can? :manage, comment.commentable.photoable.album
+      comment.user_id == user.id || can?(:manage, comment.commentable.photoable.album)
     end
     
     # Participations abilities
